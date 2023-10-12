a69d51ecb54d3a87b3509671836bc5ecfbf8e671
sunnySavlot <sunny.savlot@gmail.com>
2023-10-12 13:01:16 +0530

    "deployement process"

--- server.js
+++ server.js
@@ -1,38 +1,38 @@
-const express = require('express')
-const cors = require('cors')
-const knex = require('knex') 
-const bcrypt = require('bcrypt-nodejs')
-const register = require('./controllers/register')
-const signin = require('./controllers/signin')
-const profile = require('./controllers/profile')
-const image = require('./controllers/image')
+const express = require('express');
+const cors = require('cors');
+const knex = require('knex');
+const bcrypt = require('bcrypt-nodejs');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+const dbURI = process.env.DB_URI;
 
 const db = knex({
   client: 'pg',
-  connection: { 
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
-    host : process.env.DATABASE_HOST,
-    port : 5432,
-    user : process.env.DATABASE_USER,
-    password : process.env.DATABASE_PW,
-    database : process.env.DATABASE_DB
-  }
+  connection: dbURI, // Use the DB_URI environment variable
 });
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => db.select('*').from('users').then(users => res.json(users)))
-app.post('/signin', (req, res) => signin.handleSignin(req, res, db, bcrypt))
-app.post('/register', (req, res) => register.handleRegister(req, res, db, bcrypt))
-app.get('/profile/:id', (req, res) => profile.handleProfile(req, res, db))
-app.put('/image', (req, res) => image.handleImage(req, res, db))
+app.get('/', (req, res) => {
+  db.select('*')
+    .from('users')
+    .then((users) => res.json(users))
+    .catch((error) => res.status(500).json({ error: 'Unable to fetch users' }));
+});
 
-app.listen(3000, () => {
-  console.log("App is running of PORT:3000")
-})
+app.post('/signin', (req, res) => signin.handleSignin(req, res, db, bcrypt));
+app.post('/register', (req, res) => register.handleRegister(req, res, db, bcrypt));
+app.get('/profile/:id', (req, res) => profile.handleProfile(req, res, db));
+app.put('/image', (req, res) => image.handleImage(req, res, db));
 
-// ajay sanwlot
+app.listen(port, () => {
+  console.log(`App is running on PORT: ${port}`);
+});
